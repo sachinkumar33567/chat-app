18c0585cd1e63216ffe3a650510366b2eab6bb04
sachinkumar33567 <sachin33567@gmail.com>
2024-05-02 01:14:31 +0530

    Removed the useless app.get('/', callback) api from the index.js

--- server/index.js
+++ server/index.js
@@ -14,9 +14,9 @@ dotenv.config()
 app.use(express.json())
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-    res.send('Server is ready')
-})
+// app.get('/', (req, res) => {
+//     res.send('Server is ready')
+// })
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
