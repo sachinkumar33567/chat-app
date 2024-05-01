94d4b723347814f29f2768a31fb56d761b1ba29a
sachinkumar33567 <sachin33567@gmail.com>
2024-05-02 00:44:41 +0530

    Added some prerequisites for the deployement

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
@@ -7,6 +8,7 @@ import userRoutes from './routes/user.routes.js'
 import connectToMongoDB from './db/mongo.connect.js'
 import { app, server } from './socket/socket.js'
 
+const __dirname = path.resolve()
 dotenv.config()
 
 app.use(express.json())
@@ -19,6 +21,13 @@ app.get('/', (req, res) => {
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 app.use('/api/users', userRoutes)
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
+
 
 server.listen(3000, (req, res) => {
     connectToMongoDB()
